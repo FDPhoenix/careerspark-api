d6e6ffbd248eb8a3f2ad3a58e1a187595fb3c65a
Huynh Nhut Khang <khanghuynhfdp@gmail.com>
2025-11-29 15:16:26 +0700

    Initial commit

---
+++ src/test/java/com/khanghn/careerspark_api/CareersparkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khanghn.careerspark_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareersparkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
